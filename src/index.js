6956ff36107db6611785f09eefc950a253426ede
augustokuki <augustokukimusic@gmail.com>
2020-05-26 11:57:45 -0300

    envio do repositorio

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow, globalShortcut } = require('electron');
+
+let win;
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    // titleBarStyle: 'hidden',
+    alwaysOnTop: true,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadURL('http://google.com');
+}
+
+function createShortCuts() {
+  globalShortcut.register('CmdOrCtrl+J', toggleDevTools);
+}
+
+function toggleDevTools() {
+  win.webContents.toggleDevTools();
+}
+
+app.whenReady().then(createWindow).then(createShortCuts);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
